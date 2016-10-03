8c9e819994186846e52a1fcc4fe9154114696d74
ivan <ivan@ivan-Computer>
2016-10-03 14:16:00 +0100

    commit

--- src/Ultranet/CoreBundle/Controller/ContactController.php
+++ src/Ultranet/CoreBundle/Controller/ContactController.php
@@ -3,11 +3,37 @@
 namespace Ultranet\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
     public function indexAction()
     {
         return $this->render("UltranetCoreBundle:Contact:index.html.twig");
+    }
+
+    public function formAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            //recuperation des params de la requete
+            $name = $request->query->get('name');
+            $email = $request->query->get('email');
+            $message = $request->query->get('message');
+
+            $swiftMessage = \Swift_Message::newInstance()
+                ->setContentType('text/html')
+                ->setSubject('ConctactUser')
+                ->setFrom($email)
+                ->setTo('loickaptue@yahoo.fr')
+                ->setBody('Message venant de '.$name.' \n'.$message);
+
+            $mailer = $this->get('mailer');
+            $status = $mailer->send($swiftMessage);
+            if ($status == 0)
+                return new JsonResponse("error");
+            else
+                return new JsonResponse("success");
+        }
     }
 }
--- web/js/contact_me.js
+++ web/js/contact_me.js
@@ -14,7 +14,6 @@ $(function() {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
-            var phone = $("input#phone").val();
             var email = $("input#email").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
@@ -23,34 +22,35 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "./bin/contact_me.php",
+                url: "/contact/form",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function(data) {
+                    console.info(data);
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Votre message a été envoyé.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(data) {
+                    console.log(data);
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com;>me@example.com</a> ? Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Désolé " + firstName + " une erreur est survenue coté serveur...</strong> Ecrivez nous directement a cette adresse  <a href='mailto:me@example.com?Subject=Contact_User from myprogrammingblog.com;>me@example.com</a> Nous nous excusons pour le desagrément causé!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
